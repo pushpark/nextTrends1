f1561c70d280a8d9aa86216e55d63705d3b9152b
pushpa <t.pushpa461@gmail.com>
2023-03-18 10:40:31 +0000

    nexttrendz1

---
+++ src/components/Cart/index.js
@@ -0,0 +1,24 @@
+import Cookies from 'js-cookie'
+import {Redirect} from 'react-router-dom'
+import Header from '../Header'
+import './index.css'
+
+const Cart = () => {
+  const jwtToken = Cookies.get('jwt_token')
+  if (jwtToken === undefined) {
+    return <Redirect to="/login" />
+  }
+  return (
+    <>
+      <Header />
+      <div className="bg-container">
+        <img
+          className="image"
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+          alt="cart"
+        />
+      </div>
+    </>
+  )
+}
+export default Cart
---
+++ src/components/Products/index.js
@@ -0,0 +1,24 @@
+import Cookies from 'js-cookie'
+import {Redirect} from 'react-router-dom'
+import Header from '../Header'
+import './index.css'
+
+const Products = () => {
+  const jwtToken = Cookies.get('jwt_token')
+  if (jwtToken === undefined) {
+    return <Redirect to="/login" />
+  }
+  return (
+    <>
+      <Header />
+      <div className="bg-container">
+        <img
+          className="image"
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+          alt="products"
+        />
+      </div>
+    </>
+  )
+}
+export default Products
